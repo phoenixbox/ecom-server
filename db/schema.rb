14237f07ccfb6b2e47c59291af45bd604e704135
Shane Rogers <rogerssh@tcd.ie>
2015-09-13 15:14:45 -0700

    Update User model with devise lockable keys

--- db/schema.rb
+++ db/schema.rb
@@ -112,6 +112,9 @@ ActiveRecord::Schema.define(version: 20150912223037) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,        null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.string   "display_name"
@@ -123,5 +126,6 @@ ActiveRecord::Schema.define(version: 20150912223037) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["roles"], name: "index_users_on_roles", using: :gin
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
