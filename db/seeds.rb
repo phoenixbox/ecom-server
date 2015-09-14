a829b8142fe4f74e0a6ee579736684d86be74ecf
Shane Rogers <rogerssh@tcd.ie>
2015-09-14 00:37:14 -0700

    Default events seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+DEFAULT_EVENTS ||= YAML.load(File.open("#{Rails.root}/config/constants/default_events.yml", 'r'))
+DEFAULT_EVENTS.each do |e, i|
+  puts "Seeding event number:#{i}"
+  event = Event.new
+  event.occassion = e['occassion']
+  event.invited_count = e['invited_count']
+  event.year = e['year']
+  event.month = e['month']
+  event.day = e['day']
+  event.cancelled = e['cancelled'] || false
+  event.save
+end
